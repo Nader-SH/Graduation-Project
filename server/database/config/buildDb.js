ac3b82a6ecfd5ad0533a8e6b10f14d700ba4b3f5
Nader shak-shak <64221231+Nader-SH@users.noreply.github.com>
2025-03-01 12:33:01 +0200

    server

--- server/database/config/buildDb.js
+++ server/database/config/buildDb.js
@@ -1,4 +1,21 @@
 import { sequelize } from '../../models/index.js';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+    development: {
+        // your development config
+    },
+    production: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+};
+
+const environment = process.env.NODE_ENV || 'development';
+const dbConfig = config[environment];
 
 const buildDb = async () => {
   await sequelize.sync({ force: true });
